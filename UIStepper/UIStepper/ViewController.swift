ba901c40c3ab002064345978cecbb84acc1e4c30
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-02-27 23:57:33 +0900

    LabelとStepperの関連付け

---
+++ UIStepper/UIStepper/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  UIStepper
+//
+//  Created by home on 2018/02/27.
+//  Copyright © 2018年 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var numLabel: UILabel!
+    
+    @IBAction func changedStepperValue(_ sender: UIStepper) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
